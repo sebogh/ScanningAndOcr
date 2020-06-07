826a7ac10e45e6d37cac916f546cea232af2fe7f
Sebastian Bogan <sebogh@qibli.net>
2020-06-07 11:28:30 +0200

    deskew added

--- pdf-ocr.sh
+++ pdf-ocr.sh
@@ -4,5 +4,5 @@ PDF=$1
 TMP_PDF=`mktemp`
 TMP_TIFF=`mktemp --suffix=".tiff"`
 
-echo "convert -density 300 $PDF -alpha Off -depth 8 $TMP_TIFF && tesseract $TMP_TIFF $TMP_PDF -l deu pdf && cp $TMP_PDF $PDF"
-convert -density 300 $PDF -alpha Off -depth 8 $TMP_TIFF && tesseract $TMP_TIFF $TMP_PDF -l deu pdf && cp "${TMP_PDF}.pdf" $PDF
+echo "convert -density 300 $PDF -alpha Off -deskew 40% +repage -gravity south -depth 8 $TMP_TIFF && tesseract $TMP_TIFF $TMP_PDF -l deu pdf && cp $TMP_PDF $PDF"
+convert -density 300 $PDF -alpha Off -deskew 40% +repage -gravity south -depth 8 $TMP_TIFF && tesseract $TMP_TIFF $TMP_PDF -l deu pdf && cp "${TMP_PDF}.pdf" $PDF
